d831351c97664cc494980d056aa99159d093cfc6
Sreeram Ogirala <sreeramogirala@gmail.com>
2023-03-19 20:46:08 -0400

    image upload and download

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -14,7 +14,8 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('URL')
+    
+    fetch('http://localhost:8001/feed/post/'+postId)
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch status');
@@ -26,6 +27,7 @@ class SinglePost extends Component {
           title: resData.post.title,
           author: resData.post.creator.name,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
+          image: 'http://localhost:8001/' + resData.post.imageUrl,
           content: resData.post.content
         });
       })
